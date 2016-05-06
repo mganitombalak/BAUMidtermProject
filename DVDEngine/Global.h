e2b38961cf4826083681aeaf26181f25c912c987
yasinmuaz <ysnmzz@gmail.com@>
2016-05-06 23:48:58 +0300

    genre

--- DVDEngine/Global.h
+++ DVDEngine/Global.h
@@ -88,6 +88,78 @@ inline void compareByPrice()
 
 inline void groupByGenre()
 {
+	system("cls");
+	std::cout << string(1, 201) << string(60, 205) << string(1, 187) << endl;
+	cout << string(2, 32) << string(26, 176) << " GENRE GROUP " << string(26, 176) << string(1, 32) << endl;
+	cout << string(1, 204) << string(60, 205) << string(1, 185) << endl;
+	int genreid;
+
+	cout << "Please enter GENRE ID (type '#' to main menu):" << ends;
+	cin >> genreid;
+	if (cin.fail())
+	{
+		appMgr->ShowAndAskForMainMenu();
+		return;
+	}
+	string headers[] = { "ID","Title" };
+	Utility::PrintGridHeaders(headers);
+	switch (genreid)
+	{
+	case 1:
+		for (Movie m : appMgr->fda->getResultSet())
+		{
+			if (m.getGenre() == "Suspense")
+			{
+				m.Print();
+			}
+		}
+		break;
+	case 2:
+		for (Movie m : appMgr->fda->getResultSet())
+		{
+			if (m.getGenre() == "Foreign")
+			{
+				m.Print();
+			}
+		}
+		break;
+	case 3:
+		for (Movie m : appMgr->fda->getResultSet())
+		{
+			if (m.getGenre() == "Comedy")
+			{
+				m.Print();
+			}
+		}
+		break;
+	case 4:
+		for (Movie m : appMgr->fda->getResultSet())
+		{
+			if (m.getGenre() == "Western")
+			{
+				m.Print();
+			}
+		}
+		break;
+	case 5:
+		for (Movie m : appMgr->fda->getResultSet())
+		{
+			if (m.getGenre() == "Music")
+			{
+				m.Print();
+			}
+		}
+		break;
+	case 6:
+		for (Movie m : appMgr->fda->getResultSet())
+		{
+			if (m.getGenre() == "Drama")
+			{
+				m.Print();
+			}
+		}
+		break;
+	}
 }
 
 inline void groupCountByGenre()
